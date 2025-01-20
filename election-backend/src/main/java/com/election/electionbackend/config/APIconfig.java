7ae2fb21e8bff94a11332f66dd4dc9b28088ccc1
Eduard Ohanian <eduard.ohanian@hva.nl>
2025-01-20 22:21:40 +0100

    update backend

--- election-backend/src/main/java/com/election/electionbackend/config/APIconfig.java
+++ election-backend/src/main/java/com/election/electionbackend/config/APIconfig.java
@@ -1,18 +1,17 @@
 package com.election.electionbackend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-class WebMvcConfig implements WebMvcConfigurer {
+public class APIconfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://s3e-s205-team-3.onrender.com")  // Allow cross-origin requests from frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("https://s3e-s205-team-3.onrender.com", "http://localhost:*", "http://*.hva.nl:*")  // Voeg hier je toegestane frontend URLs toe
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
