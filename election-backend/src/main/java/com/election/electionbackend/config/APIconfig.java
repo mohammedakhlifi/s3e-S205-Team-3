3d84e6b2d4c2e3acc2837bcd41303b8d33494cff
Amine Boudchich <amine.boudchich@hva.nl>
2025-01-21 13:21:42 +0100

    Backend opnieuw proberen POGING 113

--- election-backend/src/main/java/com/election/electionbackend/config/APIconfig.java
+++ election-backend/src/main/java/com/election/electionbackend/config/APIconfig.java
@@ -10,7 +10,7 @@ public class APIconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://*.hva.nl:*", "https://election-backend-latest.onrender.com")
+                .allowedOriginPatterns("https://election-backend-latest.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
