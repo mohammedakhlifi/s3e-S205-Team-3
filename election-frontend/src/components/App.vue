1320bc58396740c33cff410e6fe728a8f4d0c752
Amine Boudchich <amine.boudchich@hva.nl>
2025-01-21 14:35:19 +0100

    Backend opnieuw proberen POGING 125 PROBEREN

--- election-frontend/src/components/App.vue
+++ election-frontend/src/components/App.vue
@@ -1,31 +1,8 @@
-<template>
-  <div class="app-container">
-    <!-- Navbar Component -->
-    <navbar></navbar>
-
-    <!-- Main Content Area (dynamically changing based on route) -->
-    <div class="content">
-      <router-view></router-view>
-    </div>
-
-    <!-- Display Backend Status -->
-    <div class="backend-status">
-      <div v-if="backendStatus">
-        <p>{{ backendStatus.message }}</p>
-      </div>
-      <div v-else>
-        <p>Loading backend status...</p>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script lang="ts">
 import { defineComponent } from 'vue';
 import navbar from "@/components/NavBar.vue";
 import axios from 'axios';
 
-// Type for BackendStatus
 interface BackendStatus {
   message: string;
 }
@@ -36,19 +13,17 @@ export default defineComponent({
   },
   data() {
     return {
-      backendStatus: null as BackendStatus | null, // Define backendStatus type
-      backendURL: "https://election-backend-latest.onrender.com"
+      backendStatus: null as BackendStatus | null,
+      backendURL:  "https://election-backend-latest.onrender.com"
     };
   },
   mounted() {
-    this.fetchBackendStatus(); // Fetch the backend status when the component is mounted
+    this.fetchBackendStatus();
   },
   methods: {
-    // Use Axios to fetch backend status
     async fetchBackendStatus() {
       try {
-        apiClient = axios.create({   baseURL: 'https://election-backend-latest.onrender.com', });
-        const response = await apiClient.get<BackendStatus>('/test');
+        const response = await axios.get<BackendStatus>(`${this.backendURL}/test`);
         this.backendStatus = response.data;
       } catch (error) {
         console.error('Error fetching backend status:', error);
